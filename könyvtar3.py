0b969600057b846d64416753ee83d2947d29711a
Persi Adam <persi.adam.2021e@ms.gdszeged.hu>
2023-02-20 09:49:44 +0100

    02202

--- könyvtar3.py
+++ könyvtar3.py
@@ -1,5 +1,12 @@
-from könyvtár_papír_alap import Konyvtar
+from könyvtár_papír_alap import KONYVTAR
 
-for _ in range(3):
-    
 
+konyvtarak = []
+for i in range(3):
+    nev = input("Adja meg a könyvtár nevét: ")
+    varos = input("Adja meg a könyvtár városát: ")
+    kotetek = int(input("Adja meg a kötetek mennyiségét(millió): "))
+    konyvtarak.append(KONYVTAR(nev,varos,kotetek))
+
+for konyvt in konyvtarak:
+    print(f"{konyvt.kotetszam()} címet kapott {konyvt.nev_cim()} {konyvt.nev_nemzetiseg()} {konyvt.KOTETEK} millió gyüjtött kötettel, {konyvt.nev_varos()}, neve {konyvt.nev_hossz()} hosszúságú")
--- könyvtár_papír_alap.py
+++ könyvtár_papír_alap.py
@@ -1,11 +1,22 @@
-class Konyvtar:
-    def __init__(self, könyvtárnev, város,menyiség):
-        self. könyvtárnev = könyvtárnev
-        self.város = város
-        self.menyiség=menyiség
-    def cím(menyiség):
-        if menyiség>50:
-            return 'ügyes gyüjtő'
+class KONYVTAR():
+    def __init__(self,nev,varos,kotetek,nemzet):
+        self.NEV = nev
+        self.VAROS = varos
+        self.KOTETEK = kotetek
+        self.NEMZET = nemzet
+    def kotetszam(self):
+        if self.KOTETEK >= 50:
+            return "ügyes gyűjtő"
         else:
-            return 'kevésbé szorgalmas'
-    
+            return "kevésbé szorgalmas"
+    def nev_cim(self):
+        return self.NEV.title()
+    def nev_varos(self):
+        return f"{self.NEV} az {self.VAROS}-ban helyezkedik el."
+    def nev_hossz(self):
+        return len(self.NEV)
+    def nev_nemzetiseg(self):
+        if str.lower(self.NEMZET) == "o":
+            return f"{self.NEV} Biblioték"
+        elif str.lower(self.NEMZET) == "b":
+            return f"{self.NEV} Library"
